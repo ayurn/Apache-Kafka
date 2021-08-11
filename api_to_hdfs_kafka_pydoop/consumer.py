0d0d7a4803e3747077f818979994b81133eb2038
ayurn <ninaweayur@gmail.com>
2021-08-11 23:45:23 +0530

    [Ayur] Refactor : adding new line code in consumer

--- api_to_hdfs_kafka_pydoop/consumer.py
+++ api_to_hdfs_kafka_pydoop/consumer.py
@@ -2,7 +2,7 @@
 @Author: Ayur Ninawe
 @Date: 2021-08-09
 @Last Modified by: Ayur Ninawe
-@Last Modified time: 16:00:30 2021-08-09
+@Last Modified time: 16:00:30 2021-08-11
 @Title : Consumer to get data from topic and save it to hdfs using pydoop.
 '''
 from pydoop import hdfs
@@ -11,7 +11,8 @@ from kafka import KafkaConsumer
 consumer = KafkaConsumer('stocklive')
 
 hdfsPath='hdfs://localhost:9000//kafkaStock/test/stockdata.txt'
+
 for message in consumer:
-    values = message.value
-    with hdfs.open(hdfsPath, 'a') as wfile:
-        wfile.write(values)
+    values = message.value.decode('utf-8')
+    with hdfs.open(hdfsPath, 'at') as f:
+        f.write(f"{values}\n")
